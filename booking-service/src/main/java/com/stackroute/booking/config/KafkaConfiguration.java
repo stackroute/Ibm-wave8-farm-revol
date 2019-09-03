11c5f2da28779dcb35b7317c4f2640e71d9742f2
sandeepg98 <sandeepg98@live.com>
2019-09-03 11:54:27 +0530

    Refactored Booking Service

--- booking-service/src/main/java/com/stackroute/booking/config/KafkaConfiguration.java
+++ booking-service/src/main/java/com/stackroute/booking/config/KafkaConfiguration.java
@@ -91,11 +91,17 @@ public class KafkaConfiguration {
     @Bean
     public ConsumerFactory<String, Consumer> consumerFactory() {
         Map<String, Object> config = new HashMap<>();
+        JsonDeserializer<Consumer> deserializer = new JsonDeserializer<>(Consumer.class);
+        deserializer.setRemoveTypeHeaders(false);
+        deserializer.addTrustedPackages("*");
+        deserializer.setUseTypeMapperForKey(true);
+
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_consumer");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), new JsonDeserializer<>(Consumer.class));
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
+//        config.put(JsonDeserializer.TRUSTED_PACKAGES, "com.stackroute.booking.model");
+        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(),deserializer);
     }
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, Consumer> kafkaListenerContainerFactory() {
