83714c16212872d8f4187d6b5fbc196fca28b56e
Sai Poojitha <pujita.pabbathi@gmail.com>
2019-09-11 12:07:39 +0530

    added modules in parent pom and removed useless folders

--- farm-revol-backend/src/main/java/com/stackroute/farmrevol/repositories/ConsumerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.stackroute.farmrevol.repositories;
-
-import com.stackroute.farmrevol.models.Consumer;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ConsumerRepository extends MongoRepository<Consumer, String> {
-
-}
--- farm-revol-backend/src/main/java/com/stackroute/farmrevol/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.stackroute.farmrevol.repositories;
-
-import com.stackroute.farmrevol.models.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, Integer> {
-
-	User findByEmail(String email);
-}
