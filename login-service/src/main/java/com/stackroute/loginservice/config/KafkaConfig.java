29c606708c6bb97c13f960dbf06701b6ffcf6fea
Vishal Pattnaik <vishalpattnaik387@gmail.com>
2019-09-20 11:52:24 +0530

    updated ip address

--- login-service/src/main/java/com/stackroute/loginservice/config/KafkaConfig.java
+++ login-service/src/main/java/com/stackroute/loginservice/config/KafkaConfig.java
@@ -1,8 +1,9 @@
 package com.stackroute.loginservice.config;
 
-import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
@@ -11,13 +12,13 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import java.util.HashMap;
 import java.util.Map;
 
-
+@Configuration
 @EnableKafka
     public class KafkaConfig {
         @Bean
         public ConsumerFactory<String, String> consumerFactory() {
             Map<String, Object> config = new HashMap<>();
-            config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+            config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.23.238.238:9092");
             config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
             config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
             config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
